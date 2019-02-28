29593ef46c4b10ef1f9e5449d269b321d69ed827
SirLich <lrkoehler77@gmail.com>
2019-02-28 17:24:11 -0600

    Pull in show-databases from lab-7 write-up

---
+++ wololo.show-databases.js
@@ -0,0 +1,24 @@
+var credentials = require('./credentials.json');
+
+var mysql=require("mysql");
+
+credentials.host="ids"
+var connection = mysql.createConnection(credentials);
+
+connection.connect(function(err){
+    if(err){
+        console.log("Problems with MySQL: "+err);
+    } else {
+        console.log("Connected to Database.");
+    }
+});
+
+connection.query('SHOW DATABASES',function(err,rows,fields){
+    if(err){
+        console.log('Error looking up databases');
+    } else {
+        console.log('Returned values were ',rows);
+    }
+});
+connection.end()
+console.log("All done now.");
