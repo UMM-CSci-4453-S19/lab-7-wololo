c4898f3a4f750dab0eed194c83eec0d4e4912dc2
SirLich <lrkoehler77@gmail.com>
2019-02-28 17:00:46 -0600

    Pull in processTables from lab-7 read-me

--- wololo.summarize-db.js
+++ wololo.summarize-db.js
@@ -21,3 +21,25 @@ connection.query('SHOW DATABASES', function(err, rows, fields) {
 
 connection.end()
 console.log("All done now");
+
+function processTables(tables,dbf){ // Asynchronous row handler
+    data[dbf] = tables.length; // Now it is set.
+    processed[dbf] = 0;        // And has not yet been used as a label.
+    for(var index in tables){
+        var tableObj = tables[index];
+        for(key in tableObj){
+            var table = tableObj[key];
+            table = dbf+"."+table;
+            var sql = 'DESCRIBE '+table;
+            connection.query(sql, (function(table,dbf){
+                return function(err,desc,fields){
+                    if(err){
+                        console.log('Error describing table '+ table);
+                    } else {
+                        processDescription(desc,table,dbf);
+                    }
+                };
+            })(table,dbf));
+        }
+    }
+}
