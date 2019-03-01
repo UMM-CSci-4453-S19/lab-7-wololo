ac5e87c102d8cfb14766d93cd00dc7638ab88f1a
SirLich <lrkoehler77@gmail.com>
2019-03-01 15:50:20 -0600

    Create starting Angular files based on tutorial

--- public/app.js
+++ public/app.js
@@ -1,6 +1,1 @@
-var express=require('express'),
-    app = express(),
-    port = process.env.PORT || 1337;
-
-app.use(express.static(__dirname + '/public'));
-app.listen(port);
+var app = angular.module('MyTutorialApp',[]);
